36d8af1b13ac65e090234c3619e396950117440c
kabildeveloper <kabildeveloper@gmail.com>
2023-06-05 13:03:05 +0530

    Fixed responsive issues

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -5,7 +5,7 @@ const SkillsCard = (props: ISkill) => {
         <div
             key={props.id}
             style={{flexBasis: "32%"}}
-            className="p-4 d-flex justify-content-center flex-column justify-content-center border shadow-sm rounded">
+            className="p-4 d-flex justify-content-center flex-column justify-content-center border shadow-sm rounded h-100">
             <div className="text-center">
                 <img alt={props.name} width={32} height={32} src={props.img}/>
                 <h6 className="mt-1 mb-2">{props.name}</h6>
